e75460fa2e644e29730e8f8066fd3a528a516aa2
Andrew Betts <abetts@ucsd.edu>
2018-05-07 20:24:28 -0700

    Working Ticker with PyQt5

--- BinanceTicker/PyQtTextScroll.py
+++ BinanceTicker/PyQtTextScroll.py
@@ -4,7 +4,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
 import time
-print(cd.getCoinData('BTC',1)) #Testing Class import
 
 class TickerWidget(QWidget):
     def __init__(self,parent=None):
@@ -14,28 +13,35 @@ class TickerWidget(QWidget):
             self.update()
         self.xint = 0
         self.timer.timeout.connect(paintme)
+        self.text = 'lmao dude'     
         self.timer.start(10)
-        
+        self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+   
+
+    def setText(self,text):
+        self.text = text
     def paintEvent(self,event):
         super().paintEvent(event)
         painter = QPainter(self)
-        fontsize = self.size().height()/5;
+        fontsize = self.size().height()/10;
         font = QFont('Arial',fontsize)
         painter.setFont(font)
         fm = painter.fontMetrics()
         origin = QPoint(self.xint,fontsize)
-        textwidth = fm.width('lmao')
-        painter.drawText(origin,'Lmao')
+        textwidth = fm.width(self.text)
+        painter.drawText(origin,self.text)
         self.scroll(textwidth)
+    
     def scroll(self,textlength):
         size = self.size()
         self.xint = self.xint + size.width()/1000
-        if self.xint+textlength > size.width():
-            self.xint = 0
+        if self.xint > size.width():
+            self.xint = -textlength
         
 if __name__ == '__main__':
     a = QApplication(sys.argv)
     w = TickerWidget()
     w.resize(500,500)
     w.show()
+    w.setText('chili')
     sys.exit(a.exec_())
