da3ac5b8e76b55f16dc159aa7e16ab8b7f4d4ef8
Andrew Betts <abetts@ucsd.edu>
2018-06-13 22:57:01 -0700

    Altered Path for display.o

--- Code/Display/showDisplay.py
+++ Code/Display/showDisplay.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 import sys
 def printDisplay(word):
-    os.chdir('/home/linaro/')
+    os.chdir('/home/linaro/electrum-smart/')
     process = subprocess.Popen(['sudo','./display.o',word],stdout=subprocess.PIPE)
     output,error = process.communicate()
